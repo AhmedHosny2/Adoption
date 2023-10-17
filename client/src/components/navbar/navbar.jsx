b3e4b8d31649f0de2f20354f45bf2d8573a0c139
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-10-17 11:51:52 +0300

    added socials animation to footer and added drag effect yo navbar logo

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -21,24 +21,24 @@ const pathVariants = {
 };
 
 const navigation = [
-	{ name: 'Dashboard', href: '#', current: true },
-	{ name: 'Team', href: '#', current: false },
-	{ name: 'Projects', href: '#', current: false },
-	{ name: 'Calendar', href: '#', current: false },
+	{ name: 'Adopt', href: '#', current: true },
+	{ name: 'Put for Adoption', href: '#', current: false },
+	{ name: 'Donate', href: '#', current: false },
+	{ name: 'Other', href: '#', current: false },
 ];
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
 }
 
-export default function Example() {
+export default function Navbar() {
 	return (
 		<motion.div
 			initial={{ y: -250 }}
 			animate={{ y: 0 }}
 			transition={{ delay: 0.2, type: 'spring', stiffness: 120 }}
 		>
-			<Disclosure as="nav" className="bg-base-200">
+			<Disclosure as="nav" className="bg-base-200 sticky">
 				{({ open }) => (
 					<>
 						<div className="mx-auto max-w-9xl px-2 sm:px-6 lg:px-8">
@@ -56,7 +56,12 @@ export default function Example() {
 									</Disclosure.Button>
 								</div>
 								<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-									<div className="flex flex-shrink-0 items-center">
+									<motion.div
+										drag
+										dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+										dragElastic={1}
+										className="flex flex-shrink-0 items-center"
+									>
 										<motion.svg
 											variants={svgVariants}
 											initial="hidden"
@@ -71,7 +76,7 @@ export default function Example() {
 												d="M226.5 92.9c14.3 42.9-.3 86.2-32.6 96.8s-70.1-15.6-84.4-58.5s.3-86.2 32.6-96.8s70.1 15.6 84.4 58.5zM100.4 198.6c18.9 32.4 14.3 70.1-10.2 84.1s-59.7-.9-78.5-33.3S-2.7 179.3 21.8 165.3s59.7 .9 78.5 33.3zM69.2 401.2C121.6 259.9 214.7 224 256 224s134.4 35.9 186.8 177.2c3.6 9.7 5.2 20.1 5.2 30.5v1.6c0 25.8-20.9 46.7-46.7 46.7c-11.5 0-22.9-1.4-34-4.2l-88-22c-15.3-3.8-31.3-3.8-46.6 0l-88 22c-11.1 2.8-22.5 4.2-34 4.2C84.9 480 64 459.1 64 433.3v-1.6c0-10.4 1.6-20.8 5.2-30.5zM421.8 282.7c-24.5-14-29.1-51.7-10.2-84.1s54-47.3 78.5-33.3s29.1 51.7 10.2 84.1s-54 47.3-78.5 33.3zM310.1 189.7c-32.3-10.6-46.9-53.9-32.6-96.8s52.1-69.1 84.4-58.5s46.9 53.9 32.6 96.8s-52.1 69.1-84.4 58.5z"
 											/>
 										</motion.svg>
-									</div>
+									</motion.div>
 									<div className="hidden sm:ml-6 sm:block">
 										<div className="flex space-x-4">
 											{navigation.map((item) => (
@@ -160,7 +165,7 @@ export default function Example() {
 																'block px-4 py-2 text-sm text-gray-700'
 															)}
 														>
-															Sign out
+															Log Out
 														</a>
 													)}
 												</Menu.Item>
